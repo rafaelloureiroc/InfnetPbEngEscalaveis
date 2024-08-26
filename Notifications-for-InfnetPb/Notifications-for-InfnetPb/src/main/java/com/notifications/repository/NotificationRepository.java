bb4ba27a900f58200bc3494956a6e1c6e88ec739
Rafael Loureiro <93484572+rafaelloureiroc@users.noreply.github.com>
2024-08-26 13:31:21 -0300

    Delete Notifications-for-InfnetPb/Notifications-for-InfnetPb directory

--- Notifications-for-InfnetPb/Notifications-for-InfnetPb/src/main/java/com/notifications/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.notifications.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.notifications.Model.Notification;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
